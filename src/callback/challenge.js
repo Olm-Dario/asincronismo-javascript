c6b38e3615e74cb933162077f8065880946a7a69
Dario <dariolmedo2@gmail.com>
2023-02-17 12:01:46 -0300

    Fetch data

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,5 +1,4 @@
-const XMLHttppRequest = require('xmlhttprequest').XMLHttpRequest;
-//llamado al XmlHttpRequest
+const XMLHttppRequest = require('xmlhttprequest').XMLHttpRequest; //llamado al XmlHttpRequest
 const API = 'https://api.escuelajs.co/api/v1'; //API en mayúscula porque es una referencia que no va a cambiar
 
 function fetchData(urlApi, callback) { //urlApi: no confundir y colocar API
@@ -10,11 +9,29 @@ function fetchData(urlApi, callback) { //urlApi: no confundir y colocar API
     if (xhttp.readyState === 4) { //si el estado ha sido completada la llamada
       if (xhttp.status === 200) { //el servido responde de forma correcta
         callback(null, JSON.parse(xhttp.responseText)); //dentro de xhttp.responseTex recibimos lo que entrega el servidor en texto y se hace la transformación en JSON
+      } else {
+        const error = new Error('Error' + urlApi);
+        return callback(error, null); //es null porque no se está regresando ningún dato
       }
-    } else {
-      const error = new Error('Error' + urlApi);
-      return callback(error, null); //es null porque no se está regresando ningún dato
     }
   }
   xhttp.send();
 }
+
+
+fetchData(`${API}/products`, (error1, data1) => {
+  if (error1) {
+    return console.error(error1);
+  }
+
+  fetchData(`${API}/products/${data1[0].id}`, (error2, data2) => {
+    if (error2) return console.error(error2);
+
+    fetchData(`${API}/categories/${data2?.category.id}`, (error3, data3) => {
+      if (error3) return console.error(error3);
+      console.log(data1[0]);
+      console.log(data2.title);
+      console.log(data3.name);
+    });
+  })
+});
