5576b90ab9bcaa2b0ee410d2ddd47515742e9c66
Dario <dariolmedo2@gmail.com>
2023-02-22 11:29:51 -0300

    Qué son las promesas

---
+++ src/promise/index.js
@@ -0,0 +1,22 @@
+const promise = new Promise((resolve, reject) => {
+  resolve('paso!')
+});
+
+const cows = 1;
+
+const countCows = new Promise((resolve, reject) => {
+  if (cows > 10) {
+    resolve(`We have ${cows} cows on the farm`);
+  } else {
+    reject('There is no cows on the farm');
+  }
+});
+
+
+countCows.then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.log(error);
+}).finally(() => {
+  console.log('finally');
+});
